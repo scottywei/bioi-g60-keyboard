917ec51a5327e1d276c5868e6713e54b6ab256b8
Zsolt Sándor <ezsolt@gmail.com>
2024-10-21 14:27:10 +0200

    Create help-f60m-sorry-for-asking-here.h

---
+++ help-f60m-sorry-for-asking-here.h
@@ -0,0 +1,7 @@
+Hello, 
+
+I am really sorry for asking help this way, but I need help in getting my f60m pcb to work with VIA.
+I can't use the VIA app because the PCB is not recognised. I have no infor. no hex, no nothing for this board.
+Can you help me pelase? my email is ezsolt@gmail.com 
+
+thank you
